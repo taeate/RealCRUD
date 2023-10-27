e794b402c18a01ca96506b63c63ecdeffc0f8e25
taeate <tjdwls3144@gmail.com>
2023-10-27 17:16:14 +0900

    home setting

---
+++ application/views/layout/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" type="text/css" href="header.css" >
+
+
+</head>
+<nav>
+    <button class="round-button">
+        <a href="">Home</a>
+    </button>
+    <div>REDERS</div>
+    <button class="round-button">
+        <a href="">LIST</a>
+    </button>
+    
+</nav>
+
